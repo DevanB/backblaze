a3b171a277840f5e6eb1788a8e6c4ba4d0ae3417
Sam Granger <sam@weprovide.com>
2015-09-24 01:09:00 +0200

    Authorize Account method added

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 /*
  * Backblaze B2
  */
+
+var curl = require('curlrequest');
+
+var B2 = function(accountId, applicationKey, options) {
+	this._accountId = accountId;
+	this._applicationKey = applicationKey;
+	
+	options = options || function(){};
+};
+
+
+B2.prototype.authorizeAccount = function() {
+    console.log(this._accountId + ':' + this._applicationKey);
+    var options = {
+        url: 'https://api.backblaze.com/b2api/v1/b2_authorize_account'
+      , user: this._accountId + ':' + this._applicationKey
+    };
+
+    curl.request(options, function (err, file) {
+        console.log(file);
+    });
+};
+
+
+exports.B2 = B2;
